3e3fabd8e29894d9d8ff722529ff80e204eca8aa
djskim27 <djs.kim27@gmail.com>
2017-11-05 00:27:24 -0400

    added create route and new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,17 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.create(post_params)
+
+        redirect_to post_path(@post)
+    end
+
+    private
+    def post_params
+        post = params.require(:post).permit(:title, :content)
+    end
 end
